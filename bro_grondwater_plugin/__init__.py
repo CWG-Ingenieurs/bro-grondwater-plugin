67935445e9dcc77242b7c7e2d39567df92ddd3e2
Mattijs Borst <m.borst@cwgi.nl>
2025-12-19 16:32:46 +0100

    move message

--- bro_grondwater_plugin/__init__.py
+++ bro_grondwater_plugin/__init__.py
@@ -10,5 +10,16 @@ def classFactory(iface):
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
-    from .bro_grondwater_plugin import BROGrondwaterPlugin
-    return BROGrondwaterPlugin(iface)
+    try:
+        from .bro_grondwater_plugin import BROGrondwaterPlugin
+        return BROGrondwaterPlugin(iface)
+    except ImportError as e:
+        from qgis.PyQt.QtWidgets import QMessageBox
+        QMessageBox.critical(
+            None,
+            "BRO Grondwater Plugin",
+            f"Missing dependency: {e}\n\n"
+            "Install via OSGeo4W Shell:\n"
+            "  pip install hydropandas pandas openpyxl matplotlib"
+        )
+        raise
